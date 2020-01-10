fef928d9a4a5312c6d36837301ce3006d6ea7e9b
Junior Rocha <rochajunior93@hotmail.com>
2020-01-09 22:19:40 -0300

    my second commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const requireDir = require('require-dir')
+require('dotenv/config')
 
 
 
@@ -12,17 +13,14 @@ app.use(cors())
 const port = 3001
 
 
-mongoose.connect('mongodb://localhost:27017/nodeApi', {
-    useNewUrlParser: true
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
 
 requireDir('./src/models')
 
-
-
 app.use('/api', require('./src/routes'))
-app.use(require("./routes"));
-
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)
